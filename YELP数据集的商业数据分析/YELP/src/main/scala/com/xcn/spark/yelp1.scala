09446cc35011e9a5f510aaebbdb45a7e6c6afea7
liangzai <liangzi@qq.com>
2020-11-04 16:49:51 +0800

    答案代码

--- YELP数据集的商业数据分析/YELP/src/main/scala/com/xcn/spark/yelp1.scala
+++ YELP数据集的商业数据分析/YELP/src/main/scala/com/xcn/spark/yelp1.scala
@@ -1,8 +1,10 @@
 package com.xcn.spark
 
-import org.apache.spark.sql.SparkSession
+import com.alibaba.fastjson.{JSON, JSONArray}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types._
 
-object yelp {
+object yelp1 {
 
   //{
 
@@ -31,9 +33,63 @@ object yelp {
 
   def main(args: Array[String]): Unit = {
     val ss = SparkSession.builder().getOrCreate()
+    val rdd = ss.sparkContext.textFile("hdfs://master:9000/xcn/input/yelp_academic_dataset_business.json")
+    val tmpRdd = rdd.map( x => {
+
+      val JSONObject = JSON.parseObject(x)
+      val business_id = JSONObject.getOrDefault("business_id","")
+      val name = JSONObject.getOrDefault("name","")
+      val address = JSONObject.getOrDefault("address","")
+
+      val city = JSONObject.getOrDefault("city","")
+
+      val state = JSONObject.getOrDefault("state","")
+      val postal_code = JSONObject.getOrDefault("postal_code","")
+      val latitude = JSONObject.getOrDefault("latitude","")
+      val longitude = JSONObject.getOrDefault("longitude","")
+      val stars = JSONObject.getOrDefault("stars","")
+      val review_count = JSONObject.getOrDefault("review_count","")
+      val is_open = JSONObject.getOrDefault("is_open","")
+
+      val attributes = JSONObject.getOrDefault("attributes","")
+      var att_take_out:AnyRef = ""
+      if( attributes != null ){
+         att_take_out = JSON.parseObject(attributes.toString).getOrDefault("RestaurantsTakeOut","none")
+      }
+      val categories =  JSONObject.getOrDefault("categories","")
+
+      val hours = JSONObject.getOrDefault("hours","")
+
+      Array(business_id,name,address,city,state,postal_code,latitude,longitude,stars,review_count,is_open,categories,att_take_out,hours)
+    }).filter( x => {
+      var result = true
+      for(  i <- 0 to 13 ){
+        if ( x(i) == null  ){
+          result = false
+        }else if( x(i).toString.trim.equals("") ){
+          result = false
+        }
+      }
+        result
+    }).map( x => Row(x(0).toString.trim,x(1).toString.trim,x(2).toString.trim,x(3).toString.trim,x(4).toString.trim,x(5).toString.trim,x(6).toString.trim.toFloat,x(7).toString.trim.toFloat,x(8).toString.trim,x(9).toString.trim,x(10).toString.trim,x(11).toString.trim.split(","),x(12).toString.trim.toLowerCase(),x(13).toString.trim) )
+
+    val tmpSchema = StructType(Array(StructField("business_id",StringType,true),StructField("name",StringType,true),StructField("address",StringType,true),StructField("city",StringType,true),StructField("state",StringType,true),
+      StructField("postal_code",StringType,true),StructField("latitude",FloatType,true),StructField("longitude",FloatType,true),StructField("stars",StringType,true),StructField("review_count",StringType,true),
+      StructField("is_open",StringType,true),StructField("categories",ArrayType(StringType,true),true),StructField("attributes",StringType,true),StructField("hours",StringType,true)) )
+    val tmpDF = ss.createDataFrame(tmpRdd,tmpSchema)
+
+    tmpDF.createOrReplaceTempView("business")
+
+    val b_etl = ss.sql("SELECT business_id, name, city, state, latitude, longitude, stars, review_count, is_open, categories, attributes FROM business").cache()
+    b_etl.createOrReplaceTempView("b_etl")
 
 
+    val outlier = ss.sql("select b1.business_id,sqrt(power(b1.latitude - b2.avg_lat,2) + power(b1.longitude - b2.avg_long,2)) as dist from b_etl b1 inner join (select state,avg(latitude) as avg_lat,avg(longitude) as avg_long from b_etl group by state) b2 on b1.state = b2.state order by dist desc")
+    outlier.createOrReplaceTempView("outlier")
+    val joined = ss.sql("select b.* from b_etl b inner join outlier o on b.business_id = o.business_id where o.dist < 10")
 
+
+    joined.write.mode("overwrite").parquet("hdfs://master:9000/xcn/output/yelp/init")
   }
 
 
--- YELP数据集的商业数据分析/YELP/src/main/scala/com/xcn/spark/yelp2.scala
+++ YELP数据集的商业数据分析/YELP/src/main/scala/com/xcn/spark/yelp2.scala
@@ -1,5 +1,83 @@
 package com.xcn.spark
 
-object yelp2 {
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
 
+object yelp2 {
+ def main(args: Array[String]): Unit = {
+  val ss = SparkSession.builder().getOrCreate()
+
+  val df = ss.read.parquet("hdfs://master:9000/xcn/output/yelp/init")
+  df.createOrReplaceTempView("business")
+  val options = Map("header"->"true","inferschema"->"true")
+
+  println("----------------------正在处理categories字段数组========>")
+  val part_business = ss.sql("select business_id,state,city,stars,review_count,explode(categories) as category from business")
+
+  //  val all = ss.sql("select business_id,state,city,stars,review_count,explode(categories) as category from business")
+  //  all.createOrReplaceTempView("all")
+
+  /* part_business.show(5)
+     +--------------------+-----+-------+-----+------------+------------------+
+     |         business_id|state|   city|stars|review_count|          category|
+     +--------------------+-----+-------+-----+------------+------------------+
+     |-2YIg-PAgCMXMLxw5...|   AZ|Phoenix|  4.0|          41|          Notaries|
+     |-2YIg-PAgCMXMLxw5...|   AZ|Phoenix|  4.0|          41|   Mailbox Centers|
+     |-2YIg-PAgCMXMLxw5...|   AZ|Phoenix|  4.0|          41| Printing Services|
+     |-2YIg-PAgCMXMLxw5...|   AZ|Phoenix|  4.0|          41|    Local Services|
+     |-2YIg-PAgCMXMLxw5...|   AZ|Phoenix|  4.0|          41|  Shipping Centers|
+     +--------------------+-----+-------+-----+------------+------------------+
+  */
+
+  val part_business1 = part_business.rdd.map( x => {
+   val result = x(5).toString.replace(" ","")
+   Row(x(0).toString,x(1).toString,x(2).toString,x(3).toString.toFloat,x(4).toString.toInt,result)
+  })
+
+  val pbSchema = StructType(Array(StructField("business_id",StringType,true),StructField("state",StringType,true),StructField("city",StringType,true),StructField("stars",FloatType,true),StructField("review_count",IntegerType,true),StructField("category",StringType,true)))
+
+  val part_business2 = ss.createDataFrame(part_business1,pbSchema)
+
+  part_business2.createOrReplaceTempView("part_business")
+
+  println("----------------------1.商业类别个数========>")
+  // 1.商业类别个数
+  val all_categories = ss.sql("select count(distinct category) as category from part_business")
+  all_categories.write.mode("overwrite").options(options).csv("/xcn/output/yelp/"+"1")
+
+  println("----------------------2.美国10种主要的商业类别========>")
+  // 2.美国10种主要的商业类别
+  val main_categories = ss.sql("select category,count(*) as num from part_business group by category order by num desc limit 10")
+  main_categories.write.mode("overwrite").options(options).csv("/xcn/output/yelp/"+"2")
+
+  println("----------------------3.每个城市各种商业类型的商家数量========>")
+  // 3.每个城市各种商业类型的商家数量
+  val top_cat_city = ss.sql("select city,category,count(business_id) as num from part_business group by city,category order by num desc")
+  top_cat_city.repartition(1).write.mode("overwrite").options(options).csv("/xcn/output/yelp/"+"3")
+
+  println("----------------------4.商家数量最多的10个城市========>")
+  // 4.商家数量最多的10个城市
+  val bus_city = ss.sql("select city,count(distinct business_id) as busOfNum from part_business group by city order by busOfNum desc limit 10")
+  bus_city.write.mode("overwrite").options(options).csv("/xcn/output/yelp/"+"4")
+
+  println("----------------------5.消费者评价最多的10种商业类别========>")
+  // 5.消费者评价最多的10种商业类别
+  val sum_cat = ss.sql("select category,count(review_count) as avg_review_count from part_business group by category order by avg_review_count desc limit 10 ")
+  sum_cat.write.mode("overwrite").options(options).csv("/xcn/output/yelp/"+"5")
+
+  println("----------------------6.最受消费者喜欢的前10种商业类型========>")
+  // 6.最受消费者喜欢的前10种商业类型
+  val sum_stars = ss.sql("select category,AVG(stars) as avgOfStars from part_business group by category order by avgOfStars desc limit 10 ")
+  sum_stars.write.mode("overwrite").options(options).csv("/xcn/output/yelp/"+"6")
+
+  println("----------------------7.商业额外业务的评价情况========>")
+  // 7.商业额外业务的评价情况
+  val for_att = ss.sql("select attributes,stars,explode(categories) as category from business")
+  for_att.createOrReplaceTempView("for_att")
+  val att = ss.sql("select attributes as RestaurantsTakeOut, avg(stars) as stars from for_att group by attributes order by stars")
+  att.repartition(1).write.mode("overwrite").options(options).csv("/xcn/output/yelp/"+"7")
+
+
+ }
 }
